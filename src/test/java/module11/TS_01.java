8d56aad0fbff3901448294a6f7c1b38ee2869982
prithivicoppa <prithivi.coppa@gmail.com>
2023-11-09 17:14:09 -0800

    Initial Commit

---
+++ src/test/java/module11/TS_01.java
@@ -0,0 +1,28 @@
+package module11;
+
+import org.testng.annotations.Test;
+
+public class TS_01 {
+			
+		@Test
+		
+	 void testcase001()
+		
+		{ System.out.println("executing module 11 TS01 test case 001");
+		}
+		
+		@Test
+		
+	public void testcase002()
+		
+		{ System.out.println("executing module 11 TS01 test case 002");
+		}
+		
+		@Test
+
+	public void testcase003()
+	
+	{ System.out.println("executing module 11 TS01 test case 003");
+	}
+}
+
